a6c0a6455eb883d257f6daaf5865325fef13f043
falcon007 <rama.shanker.pali@gmail.com>
2022-05-18 17:54:52 +0000

    manage project

--- Day2/spring-boot-app/src/main/java/com/rama/app/controller/AppController.java
+++ Day2/spring-boot-app/src/main/java/com/rama/app/controller/AppController.java
@@ -1,12 +1,27 @@
 package com.rama.app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppController {	
 	    @RequestMapping("/")
 	    public String index() {
 	        return "Hello from controller";
-	    }   
+	    }
+
+		@GetMapping("/data")
+		public String indexdata() {
+			return "getting data from controller";
+		}
+
+		@PostMapping("/post")
+		public String indexpost() {
+			return "getting post from controller";
+		}
+
+	     @PutMapping("/put")
+	     public String indexput() {
+		return "getting put from controller";
+	}
+
 }
