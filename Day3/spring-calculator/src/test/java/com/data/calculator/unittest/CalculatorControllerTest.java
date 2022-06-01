b5f86324b1786f3339b1ebb1928a773edc92bb94
Rama Shanker <rama.shanker@wirelesscar.com>
2022-06-01 21:53:57 +0200

    updated and formated code

--- Day3/spring-calculator/src/test/java/com/data/calculator/unittest/CalculatorControllerTest.java
+++ Day3/spring-calculator/src/test/java/com/data/calculator/unittest/CalculatorControllerTest.java
@@ -5,49 +5,46 @@ import com.data.calculator.MyOperation;
 import com.data.calculator.Operation;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.mockito.Matchers.anyInt;
 
 
 public class CalculatorControllerTest {
-    MyOperation myOperation=new MyOperation();
-    Operation operation=new Operation(myOperation);
-    CalculatorController calculatorController=new CalculatorController(operation);
-    @Test
-    public void additionSuccessTest(){
-       int a=5;
-       int b=6;
-       int expectedValue=11;
-        int actualValue=calculatorController.addition(a,b);
-        Assert.assertEquals(expectedValue,actualValue);
-    }
+  MyOperation myOperation = new MyOperation();
+  Operation operation = new Operation(myOperation);
+  CalculatorController calculatorController = new CalculatorController(operation);
 
-    @Test
-    public void additionFailureTest(){
-        int a=5;
-        int b=6;
-        int expectedValue=12;
-        int actualValue=calculatorController.addition(a,b);
-        Assert.assertNotEquals(expectedValue,actualValue);
-    }
+  @Test
+  public void additionSuccessTest() {
+    int a = 5;
+    int b = 6;
+    int expectedValue = 11;
+    int actualValue = calculatorController.addition(a, b);
+    Assert.assertEquals(expectedValue, actualValue);
+  }
 
-    @Test
-    public void subtractionTest(){
+  @Test
+  public void additionFailureTest() {
+    int a = 5;
+    int b = 6;
+    int expectedValue = 12;
+    int actualValue = calculatorController.addition(a, b);
+    Assert.assertNotEquals(expectedValue, actualValue);
+  }
 
-    }
+  @Test
+  public void subtractionTest() {
 
-    @Test
-    public void multiplicationTest(){}
+  }
 
-    @Test
-    public void divisionTest(){}
+  @Test
+  public void multiplicationTest() {
+  }
 
-    @Test
-    public void myOperationTest(){}
+  @Test
+  public void divisionTest() {
+  }
+
+  @Test
+  public void myOperationTest() {
+  }
 
 }
